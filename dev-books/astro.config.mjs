814b962abae4e212dfce0337e6f0df3c3a38fe0d
Diego Pasten <diegopasten78@gmail.com>
2025-04-03 00:04:34 -0300

    feat: :wrench: Configuración y Final del proyecto

--- dev-books/astro.config.mjs
+++ dev-books/astro.config.mjs
@@ -1,10 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+// import { Schema } from 'astro:schema';
 
 // https://astro.build/config
 export default defineConfig({
     vite: {
         plugins: [tailwindcss()],
-    }
+
+    },
+    output: 'server',
+    env: {
+        schema: {
+          SHOW_BUY_BUTTON: envField.boolean({ default: true, context: 'server', access: 'public' }),
+          SCORE_API_ENDPOINT: envField.string({ context: 'server', access: 'public' }),
+        }
+      },
 });
